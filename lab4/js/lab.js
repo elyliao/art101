8f64ff9ca7164fd1d0a76bf74b162b427b2c4960
elyliao <eyliao@ucsc.edu>
2023-04-23 20:10:01 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock, Paper, Scissors
+// An Implementation of the game
+// Author: Elizabeth Liao <eyliao@ucsc.edu>
+// Date: April 23, 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Creating a Rock, Paper, Scissors Game
+  // Create variables for:
+    // Rock
+    // Paper
+    // Scissors
+  // Print text: “Rock, Paper, Scissors…”
+  // Prompt user input
+  // Computer chooses variable at random
+  // Print text: “Shoot!”
+  // Print text: “User: (user’s variable), Computer: (computer’s variable)”
+  // Compares variable:
+    // Rock beats scissor
+    // Paper beats rock
+    // Scissor beats paper
+  // Print text: “Result:  (User/computer) Wins!” 
+    // Or: Print text: “Result: Draw!”
+  // Print text: “Continue?” and prompt user input
+    // If user inputs “yes”: restart from 2
+    // If the user inputs “no”: print text: “Thanks for playing!”
