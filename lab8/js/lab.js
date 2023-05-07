bfb4dd53606bac83f2c385c5b73ae3909d00b3d1
elyliao <eyliao@ucsc.edu>
2023-05-07 16:22:18 -0700

    lab8

---
+++ lab8/js/lab.js
@@ -0,0 +1,29 @@
+// index.js - Lab 8: Anon Functions and Callbacks
+// Author: Elizabeth Liao <eyliao@ucsc.edu>
+// Date: May 8, 2023
+
+// declaring array of numbers
+var nums = [13, 24, 7, 2, 10];
+console.log("This is my array of numbers: " + nums);
+
+// make function for squaring
+function square(x) {
+  var results = x * x;
+  return results;
+};
+
+// test square function
+console.log("2 squared is: " + square(2));
+console.log("4 squared is: " + square(4));
+
+// square each number in the array
+console.log("My array of numbers squared is: " + nums.map(square));
+
+// anon function for doubling
+doubledArray = nums.map(function(x) {
+  var doubled = x * 2;
+  return doubled;
+});
+
+// doubling my array
+console.log("Each of the numbers in my array doubled is: " + doubledArray);
