29c8c5c1e04d19591d91ae977dfd3a3001cd77c8
elyliao <eyliao@ucsc.edu>
2023-05-24 16:43:37 -0700

    lab 13 bonus task

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -2,9 +2,42 @@
 // Author: Elizabeth Liao <eyliao@ucsc.edu>
 // Date: May 25, 2023
 
-// create a function that loops through 1-200 and prints out either the number, fizz, buzz, boom, or fizzbuzz/fizzboom/buzzboom/fizzbuzzboom
 function fizzBuzzBoom() {
-    for (var i=1; i<=200; i++) {
+    $("#output").empty();
+    // assign input values to variables
+    var max = $("#max").val();
+    var n1 = $("#n1").val();
+    var o1 = $("#o1").val();
+    var n2 = $("#n2").val();
+    var o2 = $("#o2").val();
+    var n3 = $("#n3").val();
+    var o3 = $("#o3").val();
+    var n4 = $("#n4").val();
+    var o4 = $("#o4").val();
+    for (var i=1; i<=max; i++) {
+        var print = "";
+        // if number is a factor, add output value to string
+        if (i%n1 == 0) {
+            print += o1;
+        }
+        if (i%n2 == 0) {
+            print += o2;
+        }
+        if (i%n3 == 0) {
+            print += o3;
+        }
+        if (i%n4 == 0) {
+            print += o4;
+        }
+        // if number was not a factor of any of the given values, add number value to string
+        if (print == "") {
+            print += i;
+        }
+        // append each number/result to the div
+        $("#output").append("<p>" + print + "</p>");
+    };
+    // for original fizzbuzzboom function from 1-200
+    /* for (var i=1; i<=200; i++) {
         // multiple of 3 and 5 and 7
         if (i%105 == 0) {
             var print = "FizzBuzzBoom!";
@@ -38,10 +71,11 @@ function fizzBuzzBoom() {
         }
         // append each number/result to the div
         $("#output").append("<p>" + print + "</p>");
-    }};
+    } */
+};
 
 // button to call the function
 $("#fizz-buzz-boom").click(function () {
-    $("#fizz-buzz-boom").remove();
+    $("#fizz-buzz-boom").html("start again");
     fizzBuzzBoom();
 });
