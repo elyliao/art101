5aac15ba3be8a9ca49634539337c0f3ebb5015e7
elyliao <eyliao@ucsc.edu>
2023-06-05 20:40:24 -0700

    lab16 task x

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,6 +2,7 @@
 // Author: Elizabeth Liao <eyliao@ucsc.edu>
 // Date: June 5, 2023
 
+// initial comic 
 function getComic() {
     $.ajax({
         url: "https://xkcd.com/info.0.json",
@@ -21,3 +22,45 @@ function getComic() {
 };
 
 getComic();
+
+// function for next page
+$("#next").click(function getComic() {
+    var number = comicObj.num + 1;
+    $.ajax({
+        url: "https://xkcd.com/" + number + "/info.0.json",
+        data: {},
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            comicObj = data
+            $("#output").empty();
+            $("#output").append('<h1>' + comicObj.title + '</h1>');
+            $("#output").append('<img alt="' + comicObj.alt + '" alt ="' + comicObj.alt + '" src ="' + comicObj.img + '">');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error: ", textStatus, errorThrown);
+            $("#output").append("there are no more comics.")
+        }
+    })
+});
+
+// function for previous page
+$("#prev").click(function getComic() {
+    var number = comicObj.num - 1;
+    $.ajax({
+        url: "https://xkcd.com/" + number + "/info.0.json",
+        data: {},
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            comicObj = data
+            $("#output").empty();
+            $("#output").append('<h1>' + comicObj.title + '</h1>');
+            $("#output").append('<img alt="' + comicObj.alt + '" alt ="' + comicObj.alt + '" src ="' + comicObj.img + '">');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error: ", textStatus, errorThrown);
+            $("#output").append("there are no more comics.")
+        }
+    })
+});
